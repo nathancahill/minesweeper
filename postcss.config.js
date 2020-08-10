9e43c4c35896ed16558ded738f977e9c52ffbddf
Nathan Cahill <nathan@nathancahill.com>
2020-08-10 20:35:32 +0200

    add whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
     content: ['./src/**/*.html', './src/**/*.svelte'],
 
-    whitelistPatterns: [/svelte-/],
+    whitelistPatterns: [/svelte-/, /bg-red-500/, /cursor-pointer/, /cursor-default/],
 
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
